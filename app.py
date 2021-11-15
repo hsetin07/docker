e8be10493a2b5933470a3aae2c8fba0fefb3d934
hsetin07 <niteshsalvi0711@gmail.com>
2021-11-15 14:23:07 +0100

    apikey

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask
-import os
+# import os
 server = Flask(__name__)
-# Set environment variables
-os.environ['API_USER'] = 'username'
-os.environ['API_PASSWORD'] = 'secret'
+# # Set environment variables
+# os.environ['API_USER'] = 'username'
+# os.environ['API_PASSWORD'] = 'secret'
 
-# Get environment variables
-USER = os.getenv('API_USER')
-PASSWORD = os.environ.get('API_PASSWORD')       
-print(os.getenv('API_USER'))
-print(os.environ.get('API_PASSWORD'))
-print(os.environ)
+# # Get environment variables
+# USER = os.getenv('API_USER')
+# PASSWORD = os.environ.get('API_PASSWORD')       
+# print(os.getenv('API_USER'))
+# print(os.environ.get('API_PASSWORD'))
+# print(os.environ)
+
+
+
+
 
 @server.route("/")
 def hello():
-    return PASSWORD
+    return api_key
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0')
